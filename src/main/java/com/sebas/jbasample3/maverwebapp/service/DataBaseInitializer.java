f09f9dd57598394a5bd9f541e621aa678924030d
sebas portatil 2013 <sgerman@indra.es>
2014-08-19 08:21:10 +0200

    despues leccion 24 (añadimos tambien postgres)

--- src/main/java/com/sebas/jbasample3/maverwebapp/service/DataBaseInitializer.java
+++ src/main/java/com/sebas/jbasample3/maverwebapp/service/DataBaseInitializer.java
@@ -3,7 +3,6 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package com.sebas.jbasample3.maverwebapp.service;
 
 import com.sebas.jbasample3.maverwebapp.entity.Blog;
@@ -29,53 +28,56 @@ import org.springframework.stereotype.Service;
 @Transactional
 @Service
 public class DataBaseInitializer {
-    
+
     @Autowired
     private BlogRepository blogRepository;
-    
+
     @Autowired
     private UsuarioRepository usuarioRepository;
-    
+
     @Autowired
     private RoleRepository roleRepository;
-    
+
     @Autowired
     private ItemRepository itemRepository;
-    
+
     @PostConstruct
-    public void initDB()
-    {
+    public void initDB() {
         Role role1 = new Role("PELOTUDO");
         roleRepository.save(role1);
-        
+
         Role role2 = new Role("PUTO_AMO");
         roleRepository.save(role2);
-        
+
+        Role role3 = new Role("ROLE_ADMIN");
+        roleRepository.save(role3);
+
         List<Role> misRoles = new ArrayList<Role>();
         misRoles.add(role1);
         misRoles.add(role2);
+        misRoles.add(role3);
         Usuario usuario = new Usuario("Yo", misRoles);
+        usuario.setPassword("miclave");
         usuarioRepository.save(usuario);
-        
+
         List<Role> pelotudo1Roles = new ArrayList<Role>();
         pelotudo1Roles.add(role1);
         Usuario pelotudo1 = new Usuario("Pelotudo1", pelotudo1Roles);
         usuarioRepository.save(pelotudo1);
-        
+
         List<Role> pelotudo2Roles = new ArrayList<Role>();
         pelotudo2Roles.add(role1);
         Usuario pelotudo2 = new Usuario("Pelotudo2", pelotudo2Roles);
         usuarioRepository.save(pelotudo2);
-        
-        
+
         Blog blog = new Blog("JavavidBlog", "http://feeds.feedburner.com/javavids?format=xml", usuario);
         blogRepository.save(blog);
-        
+
         Item item1 = new Item("PrimerArticulo", new Date(), blog, "www.javavids.com");
         itemRepository.save(item1);
-        
+
         Item item2 = new Item("SegundoArticulo", new Date(), blog, "www.javavids.com");
         itemRepository.save(item2);
-        
+
     }
 }
